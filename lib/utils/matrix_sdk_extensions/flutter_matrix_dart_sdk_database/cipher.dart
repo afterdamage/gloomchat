8c6fbc9b2ec5c16f9e3efaae7e5b6a8b7f07dc5d
vastus67 <v.vasilev9005@gmail.com>
2026-05-30 03:57:08 +0300

    Disable local database encryption

--- lib/utils/matrix_sdk_extensions/flutter_matrix_dart_sdk_database/cipher.dart
+++ lib/utils/matrix_sdk_extensions/flutter_matrix_dart_sdk_database/cipher.dart
@@ -1,31 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
-
-import 'package:afterdamage/config/setting_keys.dart';
-import 'package:afterdamage/l10n/l10n.dart';
-import 'package:afterdamage/utils/client_manager.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:matrix/matrix.dart';
-
-const _passwordStorageKey = 'database_password';
-
 Future<String?> getDatabaseCipher() async {
   return null;
-}
-
-Future<void> _sendNoEncryptionWarning(Object exception) async {
-  final isStored = AppSettings.noEncryptionWarningShown.value;
-
-  if (isStored == true) return;
-
-  final l10n = await lookupL10n(PlatformDispatcher.instance.locale);
-  ClientManager.sendInitNotification(
-    l10n.noDatabaseEncryption,
-    exception.toString(),
-  );
-
-  await AppSettings.noEncryptionWarningShown.setItem(true);
 }
